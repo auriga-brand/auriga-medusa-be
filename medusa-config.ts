417cf92b1b904b90019b3777c8729aad2ced0971
shiftedworks <aurigastore@proton.me>
2025-08-02 05:18:01 -0400

    Update medusa-config.ts

--- medusa-config.ts
+++ medusa-config.ts
@@ -4,15 +4,16 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 const isProduction = process.env.NODE_ENV === "production"
 
+// Read the database URL without the ssl parameter
+const databaseUrl = process.env.DATABASE_URL;
+
+// Conditionally append '?ssl=true' based on the environment
+const correctedDatabaseUrl = isProduction ? `${databaseUrl}?ssl=false` : databaseUrl;
+
 module.exports = defineConfig({
   projectConfig: {
-    databaseUrl: process.env.DATABASE_URL,
-    // Corrected property name from 'database_extra' to 'extra'
-    extra: isProduction ? {
-      ssl: {
-        rejectUnauthorized: false
-      }
-    } : undefined,
+    // Use the corrected URL which includes the SSL parameter
+    databaseUrl: correctedDatabaseUrl,
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
